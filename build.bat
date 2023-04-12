27963d488819ac518cf364c04579258bc17ae38e
FabinoHq <fabino.hq@gmail.com>
2023-04-13 00:54:28 +0200

    Add main web assembly files

---
+++ build.bat
@@ -0,0 +1,12 @@
+:: Activate emsdk
+@CALL D:\emsdk\emsdk activate latest
+
+:: Build WOS
+@CALL emcc -std=c++17 -O3 -fno-exceptions -fno-rtti -fomit-frame-pointer ^
+    -ffunction-sections -fno-trapping-math -fno-math-errno -fno-signed-zeros ^
+    -W -Wall -pthread -lGL -s USE_PTHREADS=1 -s PROXY_TO_PTHREAD ^
+    -s MAX_WEBGL_VERSION=2 -o wos.js ^
+    main.cpp
+
+:: System pause
+PAUSE
